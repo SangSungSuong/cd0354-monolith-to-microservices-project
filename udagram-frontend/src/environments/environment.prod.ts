0a7caf4b377a7d9b73550e9bf3713ba2bea0619e
Sang Sung Suong <91944934+SangSungSuong@users.noreply.github.com>
2023-05-20 19:44:04 +0700

    fix

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://abe400a2ef18447af92b0079c7f5d360-277675474.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
